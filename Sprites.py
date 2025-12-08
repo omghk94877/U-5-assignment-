799ace8e24050405c59f532b01f6e6e4a17fd4c7
jiutian303jeff <youxizhu80@gmail.com>
2025-12-08 11:09:02 -0500

    smt

---
+++ Sprites.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,screen):
+        pygame.sprite.Sprite.__init__(self) 
+        self.screen = screen
+        self.image = pygame.draw.rectangle(screen, )
+        pass
+
+    def moving_left(self):
+        pass
+
+    def moving_right(self):
+        pass
+
+    def 
