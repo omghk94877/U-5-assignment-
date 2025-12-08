0a55dc385a9ef7351d6ad3eb021e9f123aac5ca7
jiutian303jeff <youxizhu80@gmail.com>
2025-12-08 11:10:46 -0500

    #

--- Sprites.py
+++ Sprites.py
@@ -1,10 +1,10 @@
 import pygame
 
-class Player(pygame.sprite.Sprite):
+class Paddle(pygame.sprite.Sprite):
     def __init__(self,screen):
         pygame.sprite.Sprite.__init__(self) 
         self.screen = screen
-        self.image = pygame.draw.rectangle(screen, )
+        self.lives = 3
         pass
 
     def moving_left(self):
@@ -13,4 +13,28 @@ class Player(pygame.sprite.Sprite):
     def moving_right(self):
         pass
 
-    def 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self,screen):
+        pass
+
+    def moving(self):
+        pass
+
+class Bricks(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def voilet(self):
+        pass
+
+    def red(self):
+        pass
+
+    def orange(self):
+        pass
+
+    def green(self):
+        pass
+
+    def blue(self):
+        pass
